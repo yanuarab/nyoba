dfed431ec3459bd2f832db57724c70daa0adb2e9
tugas <aldayanuar6@gmail.com>
2025-07-23 19:18:52 +0700

    Add class information output to sma2

--- sma2.java
+++ sma2.java
@@ -2,5 +2,6 @@ public class sma2 {
     public static void main(String[] args) {
         System.out.println("nama: Yanuar Alda Baran");
         System.out.println("absen: 25");
+        System.out.println("Kelas: 12 RPL 1");
     }
 }
